816f43e6ed94487a20511e25715fa8baa277a340
RicardoG1995 <r.gomez@udea.edu.co>
2025-05-21 22:07:02 -0500

    Add files via upload

---
+++ lista.cpp
@@ -0,0 +1,4 @@
+#include "lista.h"
+
+// La implementación de la clase Lista está completamente definida en el archivo de cabecera (lista.h)
+// ya que se trata de una clase template y sus métodos deben estar disponibles en tiempo de compilación.
